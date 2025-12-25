4f5933a4a10b71a8433b4dcc6bca52c5abec66b4
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2025-12-25 22:51:48 +0500

    Chapter 2 done. guessing game

---
+++ guessing_game/target/debug/deps/ppv_lite86-527fe895d6fa2827.d
@@ -0,0 +1,9 @@
+/home/ruxsorbek/RustBook/guessing_game/target/debug/deps/ppv_lite86-527fe895d6fa2827.d: /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
+
+/home/ruxsorbek/RustBook/guessing_game/target/debug/deps/libppv_lite86-527fe895d6fa2827.rmeta: /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
+
+/home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs:
+/home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs:
+/home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs:
+/home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs:
+/home/ruxsorbek/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/ruxsorbek/RustBook/guessing_game/target/debug/guessing_game: /home/ruxsorbek/RustBook/guessing_game/src/main.rs
