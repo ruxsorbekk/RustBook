82b632260344fa494bc3f57ed9b9b6580f88c354
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2025-12-26 19:28:45 +0500

    3.2 done

---
+++ section3/data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/ruxsorbek/RustBook/section3/data_types/target/debug/data_types: /home/ruxsorbek/RustBook/section3/data_types/src/main.rs
---
+++ section3/data_types/target/debug/deps/data_types-15abf2a98dcdd586.d
@@ -0,0 +1,5 @@
+/home/ruxsorbek/RustBook/section3/data_types/target/debug/deps/data_types-15abf2a98dcdd586.d: src/main.rs
+
+/home/ruxsorbek/RustBook/section3/data_types/target/debug/deps/libdata_types-15abf2a98dcdd586.rmeta: src/main.rs
+
+src/main.rs:
