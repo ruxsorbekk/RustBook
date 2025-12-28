ac0e7d31e08df9661167fe2b42605e9e6475da92
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2025-12-28 22:51:36 +0500

    4.1 done

--- chapter4/ownership/src/main.rs
+++ chapter4/ownership/src/main.rs
@@ -31,4 +31,52 @@
 //     println!("x = {x}, y = {y}"); // copying works in int, float, bool, char, and also tuple if they have the exact same type
 // }
 
+
 // ownership and functions
+// fn main(){
+//     let s = String::from("hello");
+    
+//     takes_ownership(s);
+    
+//     let x = 5;
+    
+//     makes_copy(x);
+// }
+
+// fn takes_ownership(some_string: String){
+//     println!("{some_string}");
+// }
+
+// fn makes_copy(some_integer: i32){
+//     println!("{some_integer}");
+// }
+
+
+// return values and scope
+// fn main(){
+//     let s1 = gives_ownership();
+//     let s2 = String::from("hello");
+//     let s3 = takes_and_gives_back(s2);
+// }
+
+// fn gives_ownership() -> String {
+//     let some_string = String::from("hi");
+//     some_string
+// }
+
+// fn takes_and_gives_back(a_string: String) -> String {
+//     a_string
+// }
+
+
+fn main(){
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The lenth of '{s2}' is {len}");
+}
+
+fn calculate_length(s: String) -> (String, usize){
+    let length = s.len();
+    (s, length)
+}
+
