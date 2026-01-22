e9a4d41f3731075275d6c98a25d2a3da9337f713
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-01-22 22:22:00 +0500

    7.3, 7.4 read

---
+++ chapter7/c7_3/src/main.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+use rand::Rng;
+
+fn main() {
+    // let mut map = HashMap::new();
+    // map.insert(1, 2);
+    // println!("{:?}", map);
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+}
