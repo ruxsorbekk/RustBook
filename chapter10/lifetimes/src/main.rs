9ec64a71e88aa75becfc04772ab5091bceb9457e
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-01-29 16:26:38 +0500

    10 done

---
+++ chapter10/lifetimes/src/main.rs
@@ -0,0 +1,35 @@
+// fn main() {
+//     let x = 5;
+//     let r = &x;
+//     println!("r: {r}");
+// }
+
+
+// generic lifetime annotations
+// fn main() {
+//     let string1 = String::from("abkadsjh");
+//     let string2 = String::from("yokoso watashi no soul society");
+//
+//     let result = longest(string1.as_str(), string2.as_str());
+//     println!("The longest string is '{result}'");
+// }
+//
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else { y }
+// }
+
+// struct
+struct ImportantExcerpt<'a>{
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me. some years ago...");
+    let first_sentence = novel.split('.).next().expect("Could not find");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    
+}
