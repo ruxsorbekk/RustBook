a4faf77591c1b75519590cc13a0750cecbd207ac
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-01-29 23:23:52 +0500

    part 2 done

---
+++ chapter11/part2_tests/src/lib.rs
@@ -0,0 +1,41 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String>{
+//         if 2 + 2  == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal for"))
+//         }
+//     }
+//
+//     #[test]
+//     fn it_works2() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
+
+
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    // #[ignore] // thats for ignoring tests
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+}
