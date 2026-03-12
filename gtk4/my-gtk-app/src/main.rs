937ead78fd75f973dce5de83b35ce2ff54661817
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-03-12 09:27:05 +0500

    all from gtk branch

---
+++ gtk4/my-gtk-app/src/main.rs
@@ -0,0 +1,35 @@
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk4 as gtk;
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|button| {
+        button.set_label("Hello World!");
+    });
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    window.present();
+}
