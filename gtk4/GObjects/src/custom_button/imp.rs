2771db531670ef980e29f2ef6f471598938362d4
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-03-18 13:46:40 +0500

    subclassing done

---
+++ gtk4/GObjects/src/custom_button/imp.rs
@@ -0,0 +1,37 @@
+use std::cell::Cell;
+
+use gtk4::prelude::ButtonExt;
+use gtk4 as gtk;
+use gtk::glib;
+use gtk::subclass::prelude::*;
+
+#[derive(Default)]
+pub struct CustomButton {
+    number: Cell<i32>,
+}
+
+// central trait for subclassing GObject
+#[glib::object_subclass]
+impl ObjectSubclass for CustomButton {
+    const NAME: &'static str = "MyGtkAppCustomButton";
+    type Type = super::CustomButton;
+    type ParentType = gtk::Button;
+}
+
+impl ObjectImpl for CustomButton {
+    fn constructed(&self) {
+        self.parent_constructed();
+        self.obj().set_label(&self.number.get().to_string());
+    }
+}
+
+impl WidgetImpl for CustomButton {
+    
+}
+
+impl ButtonImpl for CustomButton {
+    fn clicked(&self) {
+        self.number.set(self.number.get() + 1);
+        self.obj().set_label(&self.number.get().to_string())
+    }
+}
--- gtk4/GObjects/src/main.rs
+++ gtk4/GObjects/src/main.rs
@@ -1,83 +1,34 @@
-use glib::clone;
-use std::cell::Cell;
-use std::rc::Rc;
+mod custom_button;
+
 use gtk4 as gtk;
-use gtk::{glib, Application, Button, ApplicationWindow};
+use custom_button::CustomButton;
 use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, glib};
 
+const APP_ID: &str = "org.gtk_rs.GObjectSubclassing1";
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld";
-
-// DOES NOT COMPILE!
-fn main() -> glib::ExitCode {
-    // Create a new application
+fn main() {
+    // create a new app
     let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
+    
     app.connect_activate(build_ui);
-
-    // Run the application
-    app.run()
+    app.run();
 }
 
-fn build_ui(application: &Application) {
-    // Create two buttons
-    let button_increase = Button::builder()
-        .label("Increase")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    let button_decrease = Button::builder()
-        .label("Increase")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    // reference counted object with inner-mutability
-    let number = Rc::new(Cell::new(0));
-
-    // Connect callbacks
-    // When a button is clicked, `number` should be changed
-    let number_copy = number.clone();
-    button_increase.connect_clicked(clone!(
-        #[weak]
-        number,
-        #[weak]
-        button_decrease,
-        move |_| {
-            number.set(number.get() + 1);
-            button_decrease.set_label(&number.get().to_string());
-        }
-    ));
-    button_decrease.connect_clicked(clone!(
-        #[weak]
-        button_increase,
-        move |_| {
-            number.set(number.get()-1);
-            button_increase.set_label(&number.get().to_string());
-        }
-    ));
+fn build_ui(app: &Application) {
+    // create button
+    let button = CustomButton::new();
+    button.set_margin_top(12);
+    button.set_margin_bottom(12);
+    button.set_margin_start(12);
+    button.set_margin_end(12);
     
-    // add buttons to box
-    let gtk_box = gtk::Box::builder()
-        .orientation(gtk4::Orientation::Vertical)
-        .build();
-    gtk_box.append(&button_increase);
-    gtk_box.append(&button_decrease);
     
-
-
-    // Create a window
     let window = ApplicationWindow::builder()
-        .application(application)
+        .application(app)
         .title("My GTK App")
-        .child(&gtk_box)
+        .child(&button)
         .build();
-
-    // Present the window
+    
     window.present();
 }
